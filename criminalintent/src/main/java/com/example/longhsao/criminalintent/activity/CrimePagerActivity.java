13b7c508fe6e356283606345e8031244f0821977
longshao <hnpymalong@163.com>
2016-07-04 21:10:26 +0800

    我就是试试

---
+++ criminalintent/src/main/java/com/example/longhsao/criminalintent/activity/CrimePagerActivity.java
@@ -0,0 +1,19 @@
+package com.example.longhsao.criminalintent.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 45411_000 on 2016/7/4.
+ */
+public class CrimePagerActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
